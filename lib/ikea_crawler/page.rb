f6e77600926e408512f146537b1da19cb1f195a3
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-25 10:26:54 +0900

    引数にpermalinksを渡したら、その情報をDBに格納する機能実装

--- lib/ikea_crawler/page.rb
+++ lib/ikea_crawler/page.rb
@@ -3,21 +3,30 @@ require "active_record"
 require "activerecord-import/base"
 require "yaml"
 config = YAML.load_file('config/database.yml')
-
 ActiveRecord::Base.establish_connection(config["development"])
 ActiveRecord::Import.require_adapter('mysql2')
 
+
+
 module IkeaCrawler
-  class Page
+  class Pages
     def initialize(permalinks)
       @permalinks = permalinks
     end
+    
     attr_accessor :permalinks
 
     def persist
+      pages = []
+      @permalinks.each do |page|
+        pages << Page.new(permalink: page)
+      end
+      Page.import pages
       return true
     end
-
     
   end
+  class Page < ActiveRecord::Base
+  end
+
 end
--- spec/ikea_page_spec.rb
+++ spec/ikea_page_spec.rb
@@ -3,19 +3,24 @@ require 'spec_helper'
 describe IkeaCrawler::Page do
   before do
     permalinks = ['http://www.ikea.com/jp/ja/catalog/products/50283803/', 'http://www.ikea.com/jp/ja/catalog/products/40286388']
-    @page = IkeaCrawler::Page.new(permalinks)
+    @pages = IkeaCrawler::Pages.new(permalinks)
   end
 
   it 'should be able to setter ' do
-    expect(@page.permalinks[0]).to eq 'http://www.ikea.com/jp/ja/catalog/products/50283803/'
+    expect(@pages.permalinks[0]).to eq 'http://www.ikea.com/jp/ja/catalog/products/50283803/'
   end
 
   it 'should define persist Class method' do
-    expect(IkeaCrawler::Page.method_defined?(:persist)).to be true
+    expect(IkeaCrawler::Pages.method_defined?(:persist)).to be true
   end
 
   it 'should be valid' do
-    expect(@page.respond_to?(:persist)).to be true
+    expect(@pages.respond_to?(:persist)).to be true
+  end
+
+
+  it 'should be import permalinks into database' do
+    expect(@pages.persist()).to be true
   end
 
 
